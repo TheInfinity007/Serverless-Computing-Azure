7448c9aac6c8596a4ef763eda72cd6a558e5d1a5
Ram Chandra <ramchandra71999@gmail.com>
2021-11-04 00:53:42 +0530

    add the webhook secret with hmac sha1

--- HttpTrigger3/index.js
+++ HttpTrigger3/index.js
@@ -1,19 +1,43 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    context.log('This function is created to test the GitHub Webhooks');
+  context.log("JavaScript HTTP trigger function processed a request.");
+  context.log("This function is created to test the GitHub Webhooks");
 
-    if (req.body && req.body.pages && req.body.pages.length && req.body.pages[0].title){
-        context.res = {
-            body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: ("Invalid payload for Wiki event")
-        };
-    }
+  const hmac = Crypto.createHmac(
+    "sha1",
+    "XCS4qJtN6DpFzDsYDcJpGsof7aMtsARctG5SV4DBsjUVMocjsTt4fQ=="
+  );
+  const signature = hmac.update(JSON.stringify(req.body)).digest("hex");
+  const shaSignature = `sha1=${signature}`;
+  const gitHubSignature = req.headers["x-hub-signature"];
 
-} 
+  if (!shaSignature.localeCompare(gitHubSignature)) {
+    if (
+      req.body &&
+      req.body.pages &&
+      req.body.pages.length &&
+      req.body.pages[0].title
+    ) {
+      context.res = {
+        body:
+          "Page is " +
+          req.body.pages[0].title +
+          ", Action is " +
+          req.body.pages[0].action +
+          ", Event Type is " +
+          req.headers["x-github-event"],
+      };
+    } else {
+      context.res = {
+        status: 400,
+        body: "Invalid payload for Wiki event",
+      };
+    }
+  } else {
+    context.res = {
+      status: 401,
+      body: "Signatures don't match",
+    };
+  }
+};
