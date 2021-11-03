7b46e5fbdb96eb32ba42b9a1feebfd6022385d95
Ram Chandra <ramchandra71999@gmail.com>
2021-11-04 00:40:02 +0530

    fix: when the req.body is empty

--- HttpTrigger3/index.js
+++ HttpTrigger3/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log('This function is created to test the GitHub Webhooks');
 
-    if (req.body.pages[0].title){
+    if (req.body && req.body.pages && req.body.pages.length && req.body.pages[0].title){
         context.res = {
             body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
         };
